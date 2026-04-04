2bcf6341d2529f54193ae7a44beae78fd3701df6
sheilaveiga3-art <sheilaveiga3@gmail.com>
2026-04-04 18:13:49 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ app.use(express.urlencoded({ extended: true }));
 const PIXEL_ID = "2000845004161849";
 const ACCESS_TOKEN = "EAA51UoE82scBRBNFpaoDAJ3LXlgYcVdKZC27ndZBMuwOWLZB3cg3PnFXE8XakpbO9qxBqZCPgeZBCbm3wDUuYtymLeczz6HyMR9exZCEAJZCMbZBY3iNefJUnZCZClJShfZAIrIxmwRtXgoO6vtE4MixI5KVD7WZAPO2jyf0RcfgJo2kwk4EKS1BSQKFUdnKbL1t7PDuBAZDZD";
 const KOMMO_TOKEN = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjhkYmMxNGUxZDM2OGQ1NDc5Njg1YzM0Mzc4Nzg5OWY2MDFlODViZGFkOWUzMDg0YTQwZGYzYjhkMzFkYmMyN2M4NjhhNDQ2M2RlMTcyZjBmIn0.eyJhdWQiOiJmNjk0OWUxOC1lMjBjLTRiZGQtYmRjOS1iNjdhMDI3OTQ1ODMiLCJqdGkiOiI4ZGJjMTRlMWQzNjhkNTQ3OTY4NWMzNDM3ODc4OTlmNjAxZTg1YmRhZDllMzA4NGE0MGRmM2I4ZDMxZGJjMjdjODY4YTQ0NjNkZTE3MmYwZiIsImlhdCI6MTc3NTMzNjgwMCwibmJmIjoxNzc1MzM2ODAwLCJleHAiOjE5Mjc2NzA0MDAsInN1YiI6IjEzNDEwNDYzIiwiZ3JhbnRfdHlwZSI6IiIsImFjY291bnRfaWQiOjM0Nzg0NzQzLCJiYXNlX2RvbWFpbiI6ImtvbW1vLmNvbSIsInZlcnNpb24iOjIsInNjb3BlcyI6WyJwdXNoX25vdGlmaWNhdGlvbnMiLCJmaWxlcyIsImNybSIsImZpbGVzX2RlbGV0ZSIsIm5vdGlmaWNhdGlvbnMiXSwiaGFzaF91dWlkIjoiMTE3NjQxZGItMTc2Yi00ZDFiLTg5MmQtZWU0N2JmMTM4MGQyIiwiYXBpX2RvbWFpbiI6ImFwaS1nLmtvbW1vLmNvbSJ9.Nfl2Ibb0uj6cxOrVFCSYHNG85zBo9OM-lnZKtcj2DO-Y-QOrhVX-2mrzvlMoJ7Fjq5tADDTQwaDKMg46bNeytEDeTNg2PXi_SHwGXXeVi7HGVByJBpwB_gY9fgPem_Ndy6juPIjUn3RI3yGXtORQcKoejjH_3QJ4VGt_WZyq6BXXv8sG_OQj0JSmw0-9qLCQURXyE7BQj4GKdojJiz-QKEer2qK-xktvPAbrnWIhyhNNcJwh-lauOlbDLvwvcA4kEk6M78b5C6tbzaV8JMqwWNDNTug3C7NctV6OggssuzAGMQb6namDNceBDoV_-qmONCupJ2euAGIMvulFZJH12Q";
-const KOMMO_DOMAIN = "adrianoveiga3";
 
 const ID_LEAD = "142";
 const ID_COMPRA = "93105455";
@@ -22,26 +21,26 @@ function hashSHA256(value) {
 
 async function buscarContatoKommo(leadId) {
   try {
-    const url = "https://api-g.kommo.com/api/v4/leads/" + leadId + "?with=contacts";
-    const res = await fetch(url, {
+    const urlLead = "https://adrianoveiga3.kommo.com/api/v4/leads/" + leadId + "?with=contacts";
+    const resLead = await fetch(urlLead, {
       headers: { "Authorization": "Bearer " + KOMMO_TOKEN }
     });
-    const data = await res.json();
-    console.log("Lead Kommo:", JSON.stringify(data));
+    const dataLead = await resLead.json();
+    console.log("Lead Kommo:", JSON.stringify(dataLead));
 
-    const contactId = data._embedded && data._embedded.contacts && data._embedded.contacts[0]
-      ? data._embedded.contacts[0].id : null;
+    const contactId = dataLead._embedded && dataLead._embedded.contacts && dataLead._embedded.contacts[0]
+      ? dataLead._embedded.contacts[0].id : null;
 
     if (!contactId) return {};
 
-    const resContact = await fetch("https://api-g.kommo.com/api/v4/contacts/" + contactId, {
+    const urlContact = "https://adrianoveiga3.kommo.com/api/v4/contacts/" + contactId;
+    const resContact = await fetch(urlContact, {
       headers: { "Authorization": "Bearer " + KOMMO_TOKEN }
     });
     const contact = await resContact.json();
     console.log("Contato Kommo:", JSON.stringify(contact));
 
     let phone = null, email = null, firstName = null, lastName = null;
-
     firstName = contact.first_name || null;
     lastName = contact.last_name || null;
 
