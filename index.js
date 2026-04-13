b5eb1b43ebd349764938c33063aa82097c253666
sheilaveiga3-art <sheilaveiga3@gmail.com>
2026-04-13 18:50:34 -0300

    Update index.js

--- index.js
+++ index.js
@@ -212,4 +212,3 @@ app.get("/", (req, res) => res.send("Kommo Meta CAPI rodando"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log("Servidor rodando na porta " + PORT));
-sheilaveiga@192 ~ % 
